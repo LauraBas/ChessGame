48bdebb6ed320c7ebb75e0092e9b79739b69e2a5
Laura Bassani <bassanilaurarita@gmail.com>
2021-04-16 16:06:44 +0200

    refactoring

--- src/Pawn.java
+++ src/Pawn.java
@@ -1,5 +1,3 @@
-import java.util.HashMap;
-
 import static java.lang.Integer.parseInt;
 
 public class Pawn implements Piece {
--- src/Piece.java
+++ src/Piece.java
@@ -1,4 +1,3 @@
-import java.util.HashMap;
 
 public interface Piece {
     public boolean move();
