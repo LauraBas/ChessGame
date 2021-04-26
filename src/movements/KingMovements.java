9649ae7740f7efc0e2630d446757eb74717342d3
Laura Bassani <bassanilaurarita@gmail.com>
2021-04-26 16:50:05 +0200

    refactoring

--- src/movements/KingMovements.java
+++ src/movements/KingMovements.java
@@ -7,22 +7,38 @@ import static java.lang.Integer.parseInt;
 public class KingMovements implements Move {
     @Override
     public boolean isMovementAllowed(String position, String movement, Board board, String color) {
-        String[] positionData;
-        positionData = position.split("");
-        char x = positionData[0].charAt(0);
-        int y = parseInt(positionData[1]);
-        String[] movementData =  movement.split("");
-        char xMov = movementData[0].charAt(0);
-        int yMov =  parseInt(movementData[1]);
 
+        return canMove(
+                parseX(position),
+                parseX(movement),
+                parseY(position),
+                parseY(movement),
+                board,
+                movement,
+                color
+        );
+    }
+
+    private boolean canMove(char x, char xMov, int y, int yMov, Board board, String movement, String color) {
         if (isPositionAvailable(board, movement) && isOneMovement(x, xMov, y, yMov)) {
             return true;
         } else return isOpponentInDestination(board, movement, color) && isOneMovement(x, xMov, y, yMov);
     }
 
-    public boolean isOpponentInDestination(Board board, String movement, String color) {
+    private char parseX(String position) {
+        String[] positionData = position.split("");
+        return positionData[0].charAt(0);
+    }
+
+    private int parseY(String position) {
+        String[] positionData = position.split("");
+        return parseInt(positionData[1]);
+    }
+
+    private boolean isOpponentInDestination(Board board, String movement, String color) {
         return !board.getColorAtSquare(movement).equals(color);
     }
+
     private boolean isPositionAvailable(Board board, String movement) {
         return board.isSquareEmpty(movement);
     }
